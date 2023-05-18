9756d2e3651bbe712f46359ec6a549d5607bfd34
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-18 12:50:45 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 
 // middleware
-app.use(cors())
-app.use(express.json())
+ app.use(cors())
+ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Avenger Toy server is running!')
