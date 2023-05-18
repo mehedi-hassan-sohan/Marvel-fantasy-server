0405cb58e606188ad6d67ce62f3c8c6e0927cd1f
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-05-18 12:48:22 +0600

    file setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000 
+const cors = require('cors')
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Avenger Toy server is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
